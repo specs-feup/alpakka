204d4ecae6ec9d15d53facba7233cdcdcdb483bf
GustavoASantos <64141391+GustavoASantos@users.noreply.github.com>
2024-03-25 20:22:32 +0000

    [SmaliAst] Initial work on building an apk from smali

--- SmaliAst/src/pt/up/fe/specs/smali/ast/App.java
+++ SmaliAst/src/pt/up/fe/specs/smali/ast/App.java
@@ -13,6 +13,7 @@
 
 package pt.up.fe.specs.smali.ast;
 
+import java.io.File;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -20,6 +21,13 @@ import java.util.Map;
 import org.suikasoft.jOptions.Datakey.DataKey;
 import org.suikasoft.jOptions.Datakey.KeyFactory;
 import org.suikasoft.jOptions.Interfaces.DataStore;
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.Yaml;
+
+import brut.apktool.Main;
+import brut.common.BrutException;
+import pt.up.fe.specs.util.SpecsIo;
+import pt.up.fe.specs.util.SpecsLogs;
 
 public class App extends SmaliNode {
 
@@ -28,6 +36,46 @@ public class App extends SmaliNode {
 
     public App(DataStore data, Collection<? extends SmaliNode> children) {
         super(data, children);
+    }
+
+    public File buildApk() {
+        var outputFolder = SpecsIo.mkdir("output");
+
+        for (var child : getChildren()) {
+            if (child instanceof ClassNode) {
+                var classPath = ((ClassNode) child).getDexClassName() + "/"
+                        + ((ClassNode) child).getClassDescriptor().getPackageName() + "/"
+                        + ((ClassNode) child).getClassDescriptor().getClassName();
+                var file = new File(outputFolder, classPath + ".smali");
+                SpecsIo.write(file, child.getCode());
+
+            } else if (child instanceof Resource) {
+                var file = ((Resource) child).getFile();
+                var filePath = file.getPath();
+                var path = filePath.substring(filePath.indexOf(File.separator) + 1);
+                SpecsIo.copy(file, new File(outputFolder, path));
+
+            }
+        }
+
+        var attributes = get(ATTRIBUTES);
+        var options = new DumperOptions();
+        options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+        var yaml = new Yaml(options);
+        var apktoolYml = new File(outputFolder, "apktool.yml");
+        SpecsIo.write(apktoolYml, yaml.dump(attributes));
+
+        String[] commands = { "b", outputFolder.getAbsolutePath(), "-o", "output.apk" };
+
+        try {
+            Main.main(commands);
+        } catch (BrutException e) {
+            SpecsLogs.warn("Could not build apk: " + e.getMessage());
+        }
+
+        SpecsIo.deleteFolder(outputFolder);
+
+        return new File("output.apk");
     }
 
     @Override
