82b79f57e24f031527f4e798aa2a1934e1faa492
João Bispo <jbispo@fe.up.pt>
2026-03-27 22:37:36 +0000

    [AlpakkaParser] Adds AnnotationDirective tests

---
+++ AlpakkaParser/test/pt/up/fe/specs/alpakka/parser/SmaliAstNodesTest.java
@@ -0,0 +1,36 @@
+package pt.up.fe.specs.alpakka.parser;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import pt.up.fe.specs.util.SpecsIo;
+
+import java.io.File;
+
+public class SmaliAstNodesTest extends SmaliAstTester {
+
+    public SmaliAstNodesTest() {
+        super("pt/up/fe/specs/alpakka/nodes/");
+    }
+
+    @AfterAll
+    static void clear() {
+        File outputFolder = SpecsIo.mkdir(getOutputFoldername());
+        SpecsIo.deleteFolderContents(outputFolder);
+        outputFolder.delete();
+    }
+
+    @Test
+    void testAnnotationDirective1() {
+        testSmaliFile("AnnotationDirective1.smali");
+    }
+
+    @Test
+    void testAnnotationDirective2() {
+        testSmaliFile("AnnotationDirective2.smali");
+    }
+
+    @Test
+    void testAnnotationDirective3() {
+        testSmaliFile("AnnotationDirective3.smali");
+    }
+}
